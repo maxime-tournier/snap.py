e5fb0f0faaf704c9b08d537afdcd6103cef8d19d
Maxime Tournier <maxime.tournier@anatoscope.com>
2019-03-07 16:18:30 +0100

    python3/pyside2

--- snap/qt.py
+++ snap/qt.py
@@ -1,32 +1,14 @@
-from __future__ import print_function, absolute_import 
+from PySide2 import QtCore, QtGui, QtOpenGL
+from PySide2.QtWidgets import QApplication
 
-try:
-    from PySide import QtCore, QtGui, QtOpenGL
-    from PySide.QtGui import QApplication
-    
-    def connect(src, sig, dst):
-	    src.connect(src, QtCore.SIGNAL(sig), dst)
-        
-    def signal():
-	    return QtCore.Signal()
+def connect(src, sig, dst):
+    src.connect(src, QtCore.SIGNAL(sig), dst)
 
-    def wheel_angle(ev):
-        return ev.delta()
-    
-except ImportError as e:
-    import PyQt5
-    from PyQt5 import QtCore, QtGui, QtOpenGL, QtWidgets
-    from PyQt5.QtWidgets import QApplication
+def signal():
+    return QtCore.Signal()
 
-    def connect(src, sig, dst):
-        name = sig.split('(')[0]
-        getattr(src, name).connect(dst)
-
-    def signal():
-        return QtCore.pyqtSignal()
-
-    def wheel_angle(ev):
-        return ev.angleDelta().y()
+def wheel_angle(ev):
+    return ev.delta()
     
         
 from contextlib import contextmanager
