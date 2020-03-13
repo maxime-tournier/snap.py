0fdfb793d0c325e71034d0c6c8f7fe8841fb32c9
Maxime Tournier <maxime.tournier@anatoscope.com>
2020-03-13 15:17:41 +0100

    proper defaults

--- test.py
+++ test.py
@@ -37,21 +37,25 @@ def loop():
        
 
 class Camera:
-    __slots__ = ('fov', 'ratio', 'znear', 'zfar', 'target', 'frame', 'width', 'height')
+    __slots__ = ('fov', 'znear', 'zfar', 'target', 'frame', 'width', 'height')
 
     def __init__(self):
         self.znear = 0.1
         self.zfar = 100.0
 
         self.fov = 60
-        self.ratio = 1.0
 
         self.target = np.zeros(3)
         self.frame = Rigid3()
 
         self.width, self.height = default_size
 
-
+        self.frame.center[2] = 1
+        
+    @property
+    def ratio(self):
+        return self.width / self.height
+        
     def projection(self):
         gluPerspective(self.fov, self.ratio, self.znear, self.zfar)
 
@@ -64,7 +68,8 @@ class Camera:
     def resize(self, width, height):
         self.width = width
         self.height = height
-
+        glViewport(0, 0, width, height)
+        self.ratio = width / float(height if height != 0.0 else 1.0)
         
     @coroutine
     def mouse_move(self):
@@ -76,16 +81,24 @@ class Camera:
             current = yield delta
             delta = np.array(current) - np.array(init)
             # TODO normalize delta in camera coordinates
+
+    @property
+    def distance(self):
+        return np.linalg.norm(self.target - self.frame.center)
             
-        
+    translate_factor = 4.0
+            
     def mouse_translate(self):
         init = self.frame.copy()
         mouse_move = self.mouse_move()
+
+        factor = self.translate_factor * self.distance
         
         while True:
             info = yield None
             dx, dy = mouse_move.send(info)
-            self.frame.center -= self.frame.orient(vec(dx / self.width, -dy / self.height, 0))
+            self.frame.center = init.center - init.orient(vec(factor * dx / self.width,
+                                                              -factor * dy / self.height, 0))
 
             
     def mouse_left(self):
@@ -138,7 +151,10 @@ def camera_loop():
 init()
 glClearColor(1, 0, 0, 1)
 glEnable(GL_LIGHTING)
-
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_LIGHTING)
+glEnable(GL_LIGHT0)
+        
 for events in camera_loop():
     for ev in events:
         if ev.type == pg.KEYDOWN:
